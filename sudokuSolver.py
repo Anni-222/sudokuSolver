e115dafc2d3523ceefb21b604a008c972019c018
Anni-222 <234969689+Anni-222@users.noreply.github.com>
2025-10-26 20:29:47 -0400

    sudoku solver custom board added

--- sudokuSolver.py
+++ sudokuSolver.py
@@ -2,12 +2,7 @@ import random
 import console
 import time
 
-def createBoard():
-	#num = int(input("Please enter a number from 1 to 9. "))
-	#board = [[[num, 0, 0], [0, 0, 0], [0, 0, 0]], [[0, 0, num], [0, 0, 0], [0, 0, 0]], [[0, 0, 0], [0, num, 0], [0, 0, 0]], [[0, 0, 0], [0, 0, 0], [0, num, 0]], [[0, 0, 0], [0, 0, 0], [0, 0, 0]], [[0, 0, 0], [0, 0, 0], [0, 0, 0]], [[0, 0, 0], [0, 0, 0], [0, 0, 0]], [[0, 0, 0], [0, 0, 0], [num, 0, 0]], [[0, 0, 0], [0, 0, 0], [0, 0, num]]]
-	
-	
-	
+def createBoard():	
 	board = [
 		
 		[5, 3, 0, 0, 7, 0, 0, 0, 0],
@@ -46,6 +41,32 @@ def createBoard():
 	return board
 	
 
+def makeCustomBoard():
+	boardFirst = input("Please enter your board without any spaces or symbols, and 0 for nothing.")
+	if len(boardFirst) != 81:
+		print("Incorrect board size! Boards must be 9x9.")
+		time.sleep(3)
+		return None
+	try:
+		boardTwo = int(boardFirst)
+	except:
+		print("Your board has symbols or letters in it! ")
+		time.sleep(3)
+		return None
+	board = formatBoard(boardFirst)
+	return board
+	
+def formatBoard(board):
+	boardList = []
+	boardRow = []
+	for i in range(9):
+		for j in range(9):
+			boardRow.append(board[j])
+		boardList.append(boardRow)
+	
+	return boardList
+
+
 def printBoard(board):
 	console.clear()
 	for r in range(9):
@@ -65,22 +86,6 @@ def printBoard(board):
 
 
 def findEmpty(board):
-	"""	row = random.randint(0, 8)
-	col = random.randint(0, 8)
-	val = random.randint(1, 9)
-	#print("Start")
-	#print(row)
-	#print(col)
-	if board[row][col] == 0:
-		if isValid(board, row, col, val):
-			#print("True")
-			#time.sleep(1)
-			board[row][col] = val
-			#printBoard(board)
-			return True
-	return False
-	"""
-	
 	for r in range(9):
 		for c in range(9):
 			if board[r][c] == 0:
@@ -156,19 +161,26 @@ def solve(board):
 	
 	
 	
-	
-	
-board = createBoard()
+customBoard = input("Would you like to make your own board? (y/n)").lower()
+if 'y' in customBoard:
+	board = makeCustomBoard()
+	if not board:
+		print("Board does not work!")
+		print("Using different board...")
+		time.sleep(4)
+		board = createBoard()
+else:	
+	board = createBoard()
 printBoard(board)
+time.sleep(2)
 
 works = solve(board)
-
+	
 if works:
-	time.sleep(4)
 	console.clear()
 	print("Final:")
 	time.sleep(1)
 	printBoard(board)
-
+	
 else:
 	print("Board unsolvable!")
